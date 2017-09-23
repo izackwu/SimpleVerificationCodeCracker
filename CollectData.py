3d08f917c8a75e92728d96bac7a2c43447be45d5
keithnull <799027522@qq.com>
2017-09-23 21:07:49 +0800

    Finish the code for collecting data.

--- CollectData.py
+++ CollectData.py
@@ -1,1 +1,15 @@
-import request
+#coding=utf-8
+
+import requests
+
+vcodeSource="http://****.com/inc/checkcode.asp"
+num=int(input("Please enter how many vcodes are to be downloaded:"))
+for i in range(0,num):
+    req=requests.get(vcodeSource)
+    filename="RawVcode/"+str(i)+".jpg"
+    with open(filename,"wb") as file:
+        file.write(req.content)
+print("Done!\n")
+
+
+
