8bae1cf01b8b476b7468c9d3dcea34bfea29c030
keithnull <799027522@qq.com>
2017-10-13 22:50:09 +0800

    Rewritten the part of classification.

--- Classify.py
+++ Classify.py
@@ -1,28 +1,46 @@
 #coding=utf-8
 
 from PIL import Image
+import configparser
 import os
-import Preprocess
+import CollectAndPreprocess
 import glob
 import shutil
 
 if __name__ == '__main__':
-    filePath="CuttedVcode/"
-    allUnclassifiedImages=glob.iglob(filePath + "*.jpg")
-    for imagePath in allUnclassifiedImages:
-        #print(imagePath)
-        unclassifiedImage=Image.open(imagePath)
-        tempImage=unclassifiedImage.point(lambda x : x>140 )
-        Preprocess.binImagePrinter(tempImage,mode = "special")
-        #Preprocess.binImagePrinter(unclassifiedImage)
-        while True:
-            try:
-                content=input("\nPlease input the content of this image:")
-                shutil.move(imagePath, "ClassifiedVcode/" + content[0] + "/")
-            except Exception:
-                print("\nThere's something wrong,please try again.(Enter Ctrl + C to exit)")
-            else:
-                break
+    config=configparser.ConfigParser()
+    config.read("config.ini")
+    unclassifiedFolder=config.get("preprocess","folder")
+    classifiedFolder=config.get("classify","folder")
+    allUnclassifiedImages=glob.iglob(unclassifiedFolder + "\*.jpg")
+    if os.path.exists(classifiedFolder)==False:
+        os.mkdir(classifiedFolder)
+    successNum=0
+    try:
+        for imagePath in allUnclassifiedImages:
+            unclassifiedImage=Image.open(imagePath)
+            tempImage=unclassifiedImage.point(lambda x : x>140 )
+            CollectAndPreprocess.binImagePrinter(tempImage,mode = "special")
+            while True:
+                try:
+                    character=input("\nPlease input the character in this image:")
+                    targetPath=classifiedFolder + "/" + character[0].upper() + "/"
+                    if os.path.exists(targetPath)==False:
+                        os.mkdir(targetPath)
+                    shutil.move(imagePath, targetPath)
+                except Exception:
+                    print("\nThere's something wrong,please try again.(Enter Ctrl + C to exit)")
+                else:
+                    successNum+=1
+                    break
+    except: #If enter Ctrl+C to quit.
+        print("\nOkay,let's have a break now.")
+        print("You have classified {0} characters this time.".format(successNum))
+    else:   #If finish all the classification.
+        print("\nWell done! You have classified all the characters!")
+        print("Now,Please run GetFeatureAndTrain.py")
+
+
 
 
 
